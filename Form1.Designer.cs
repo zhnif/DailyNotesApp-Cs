9626b2a37daa52aa53deee9673938de12a89b81d
zhnif <zaimsda@gmail.com>
2026-06-30 17:23:22 +0700

    Daily Notes App

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace DailyNotesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        // Ui sederhana
+        private System.Windows.Forms.TextBox titleTxt;
+        private System.Windows.Forms.TextBox contentTxt;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.DataGridView notesGrid;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+
+            this.titleTxt = new System.Windows.Forms.TextBox();
+            this.contentTxt = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.notesGrid = new System.Windows.Forms.DataGridView();
+
+            ((System.ComponentModel.ISupportInitialize)(this.notesGrid)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+            // titleTxt
+            // 
+            this.titleTxt.Location = new System.Drawing.Point(20, 20);
+            this.titleTxt.Name = "titleTxt";
+            this.titleTxt.Size = new System.Drawing.Size(250, 20);
+            this.titleTxt.TabIndex = 0;
+            // 
+            // contentTxt
+            // 
+            this.contentTxt.Location = new System.Drawing.Point(20, 60);
+            this.contentTxt.Multiline = true;
+            this.contentTxt.Name = "contentTxt";
+            this.contentTxt.Size = new System.Drawing.Size(250, 100);
+            this.contentTxt.TabIndex = 1;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(20, 170);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(120, 30);
+            this.saveBtn.TabIndex = 2;
+            this.saveBtn.Text = "Simpan Catatan";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // notesGrid
+            // 
+            this.notesGrid.AllowUserToAddRows = false;
+            this.notesGrid.AllowUserToDeleteRows = false;
+            this.notesGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.notesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.notesGrid.Location = new System.Drawing.Point(300, 20);
+            this.notesGrid.Name = "notesGrid";
+            this.notesGrid.ReadOnly = true;
+            this.notesGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.notesGrid.Size = new System.Drawing.Size(300, 200);
+            this.notesGrid.TabIndex = 3;
+            this.notesGrid.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.notesGrid_CellDoubleClick);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 260);
+            this.Controls.Add(this.titleTxt);
+            this.Controls.Add(this.contentTxt);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.notesGrid);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Daily Notes App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+
+            ((System.ComponentModel.ISupportInitialize)(this.notesGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyNotesApp
+{
+    public partial class Form1 : Form
+    {
+        // Connection string ke database lokal
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Hp\\Documents\\NotesDB.mdf;Integrated Security=True;Connect Timeout=30";
+        public Form1()
+        {
+            InitializeComponent();
+            LoadNotes();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO Notes (Title, Content) VALUES (@title, @content)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@title", titleTxt.Text);
+                cmd.Parameters.AddWithValue("@content", contentTxt.Text);
+                cmd.ExecuteNonQuery();
+            }
+            MessageBox.Show("Catatan berhasil disimpan!");
+            LoadNotes();
+            titleTxt.Clear();
+            contentTxt.Clear();
+        }
+
+        private void LoadNotes()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT Id, Title, CreatedAt FROM Notes ORDER BY CreatedAt DESC";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                notesGrid.DataSource = dt;
+            }
+        }
+
+        private void notesGrid_CellDoubleClick( object sender, DataGridViewCellEventArgs e)
+        {
+            int id = Convert.ToInt32(notesGrid.Rows[e.RowIndex].Cells["Id"].Value);
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT Content FROM Notes Where Id=@id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                string content = cmd.ExecuteScalar().ToString();
+                MessageBox.Show(content, "Isi Catatan");
+            }
+        }
+    }
+}
